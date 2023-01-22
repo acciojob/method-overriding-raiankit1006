7f58a3c255ea29804fccf0ea2f9ef72c0d6ef5a8
raiankit1006 <115402024+raiankit1006@users.noreply.github.com>
2023-01-22 22:11:53 +0530

    1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args)
+    {
+        B b = new B();
+        System.out.println(b.meth());
+        B b1 = new B();
+        System.out.println(b1.meth());
+    }
+
 }
