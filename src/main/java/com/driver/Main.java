2ad5fbceb71e13a70f44bfddda29d7b8aa44dc02
raiankit1006 <115402024+raiankit1006@users.noreply.github.com>
2023-01-27 21:07:39 +0530

    2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+ class A {
+
+    public String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
+ class B extends A {
+
+     public String meth() {
+         return "Method is overridden in Extendend class B";
+     }
+ }
+
 public class Main {
 
     public static void main(String[] args)
